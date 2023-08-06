49185893707c59f0de249c895d13149b68c7f7c4
Myawk0 <89804841+Myawk0@users.noreply.github.com>
2023-08-06 23:27:07 +0300

    add working app

---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Мявкo on 6.08.23.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+}
---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Мявкo on 6.08.23.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
